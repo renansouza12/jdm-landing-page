598a744c58c6b91ab149cbddb002a007167c5622
RenanSouza <renansouza0122@gmail.com>
2024-12-14 23:48:02 -0300

    feat: add hero component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
