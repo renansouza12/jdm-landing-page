ec21d1ac8654be6608f6a71c776fb881533e38a2
RenanSouza <renansouza0122@gmail.com>
2024-12-16 23:04:13 -0300

    feat:add about component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { CommunityComponent } from './components/community/community.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeroComponent],
+  imports: [RouterOutlet,HeroComponent,AboutComponent,CommunityComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  imports: [NgOptimizedImage],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss'
+})
+export class AboutComponent {
+
+}
