0498d67d77b7ad90e754153301a45cbeda94264c
renan <renansouza0122@gmail.com>
2024-12-19 00:21:16 -0300

    feat: add  history component

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,8 +1,9 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-history',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss'
 })
