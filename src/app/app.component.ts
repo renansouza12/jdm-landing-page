0814eb60c578158c1a5ac8cdd727c286ac43fdcb
renan <renansouza0122@gmail.com>
2024-12-20 00:16:12 -0300

    feat: add footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HeroComponent } from './components/hero/hero.component';
 import { AboutComponent } from './components/about/about.component';
 import { CommunityComponent } from './components/community/community.component';
 import { HistoryComponent } from './components/history/history.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeroComponent,AboutComponent,CommunityComponent,HistoryComponent],
+  imports: [RouterOutlet,HeroComponent,AboutComponent,CommunityComponent,HistoryComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
